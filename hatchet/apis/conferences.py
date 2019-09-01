9e5029a008dedd00a860463b2a82c84454fcbea5
exley <exleym@gmail.com>
2019-09-01 03:20:26 -0400

    add generic CRUD queries

--- hatchet/apis/conferences.py
+++ hatchet/apis/conferences.py
@@ -1,11 +1,7 @@
 from flask_restplus import Namespace, Resource, fields
+import hatchet.db.crud.core as queries
+import hatchet.db.models as db
 
-from hatchet.db.crud.conferences import (
-    edit_conference,
-    list_conferences,
-    persist_conference,
-    remove_conference_by_id
-)
 
 api = Namespace("conferences", description="conference related operations")
 
@@ -19,10 +15,35 @@ conference = api.model("Conference", {
 })
 
 
-class Conferences(Resource):
+@api.route("/conferences")
+class ConferenceCollection(Resource):
+    @api.doc("list conferences")
     @api.marshal_with(conference)
     def get(self):
-        return list_conferences()
+        return queries.list_resources(db.Conference)
 
+    @api.doc("create conference")
+    @api.expect(conference)
     @api.marshal_with(conference)
     def post(self):
+        return queries.create_resource(api.payload, db.Conference)
+
+
+@api.route("/conferences/<int:id>")
+class Conferences(Resource):
+
+    @api.doc("get conference by id")
+    @api.marshal_with(conference)
+    def get(self, id: int):
+        return queries.get_resource(id=id, model=db.Conference)
+
+    @api.doc("update conference")
+    @api.expect(conference)
+    @api.marshal_with(conference)
+    def put(self, id: int):
+        return queries.update_resource(id=id, data=api.payload, model=db.Conference)
+
+
+    @api.doc("delete conference")
+    def delete(self, id: int):
+        return queries.remove_resource(id=id, model=db.Conference)
