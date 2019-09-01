45ee9b1dc89c845347e993413750f125a63cbf3e
Exley McCormick <exleym@gmail.com>
2019-09-01 12:09:52 -0400

    clean up conflicts in generic CRUD features

--- hatchet/db/crud/base.py
+++ hatchet/db/crud/base.py
@@ -1,37 +1,39 @@
 import logging
 from typing import List
-
 from hatchet.extensions import db
-from hatchet.errors import throw_mre
+from hatchet.errors import MissingResourceException
+
 
 logger = logging.getLogger(__name__)
+ModelType = type(db.Model)
 
 
-def persist_resource(data: dict, model_class: type(db.Model)) -> db.Model:
-    obj = model_class(**data)
+def persist_resource(data: dict, model: ModelType) -> db.Model:
+    obj = model(**data)
     db.session.add(obj)
     db.session.commit()
     return obj
 
 
-def list_resources(model_class: type(db.Model)) -> List[db.Model]:
-    return model_class.query.all()
+def list_resources(model: ModelType) -> List[db.Model]:
+    return model.query.all()
 
 
-def get_resource(id: int, model_class: type(db.Model)) -> db.Model:
-    obj = model_class.filter_by(id=id).first()
+def get_resource(id: int, model: ModelType) -> db.Model:
+    obj = model.filter_by(id=id).first()
     if not obj:
-        logger.error(f"error looking up {model_class.__name__} with id={id}")
-        throw_mre(id, model_class)
+        logger.error(f"error looking up {model.__name__} with id={id}")
+        msg = f"No {model.__name__} with id={id}"
+        raise MissingResourceException(msg)
     return obj
 
 
-def search(filters: List[dict], model_class: type(db.Model)) -> List[db.Model]:
+def search(filters: List[dict], model: ModelType) -> List[db.Model]:
     return []
 
 
-def edit_resource(id: int, data: dict, model_class: type(db.Model)) -> db.Model:
-    obj = get_resource(id=id, model_class=model_class)
+def edit_resource(id: int, data: dict, model: ModelType) -> db.Model:
+    obj = get_resource(id=id, model=model)
     _ = data.pop("id", None)
     for k, v in data.items():
         setattr(obj, k, v)
@@ -40,7 +42,7 @@ def edit_resource(id: int, data: dict, model_class: type(db.Model)) -> db.Model:
     return obj
 
 
-def remove_resource_by_id(id: int, model_class: type(db.Model)):
-    obj = get_resource(id=id, model_class=model_class)
+def remove_resource_by_id(id: int, model: ModelType):
+    obj = get_resource(id=id, model=model)
     db.session.delete(obj)
     db.session.commit()
