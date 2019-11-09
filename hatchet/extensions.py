01a2814a04dd65c1c83148dfa9d9b6cc4a480ed3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-09 22:02:07 +0000

    Deploy exleym/Hatchet to github.com/exleym/Hatchet.git:gh-pages

--- hatchet/extensions.py
+++
@@ -1,5 +0,0 @@
-from flask_filter import FlaskFilter
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-filtr = FlaskFilter()
--- wsgi.py
+++
@@ -1,9 +0,0 @@
-from hatchet.app import create_app
-
-ENV = "dev-stable"
-
-
-app = create_app(env=ENV)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
